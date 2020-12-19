ccf726505fe1910d4fc6d6bc6496d4b88a5e4575
wilson-romero <me@wilsonromero.com>
2020-12-19 11:28:15 -0500

    fix: remove permission_classes

--- jobs/views.py
+++ jobs/views.py
@@ -8,9 +8,10 @@ from jobs.models import Category, Country
 class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
+
 
 class CountryList(generics.ListCreateAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
