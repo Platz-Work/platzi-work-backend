84589cfe1344156c8a6b0f42b40d91c8e008bf30
wilson-romero <me@wilsonromero.com>
2020-12-19 11:40:09 -0500

    chore: add jobs endpoint

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from jobs.views import CategoryList, CountryList
+from jobs.views import CategoryList, CountryList, JobOfferList
 
 urlpatterns = [
     path('categories/', CategoryList.as_view(), name='categories'),
     path('countries/', CountryList.as_view(), name='countries'),
+    path('jobs/', JobOfferList.as_view(), name='jobs'),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,8 +1,8 @@
-from jobs.serializers import CategorySerializer, CountrySerializer
+from jobs.serializers import CategorySerializer, CountrySerializer, JobOfferSerializer
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
-from jobs.models import Category, Country
+from jobs.models import Category, Country, JobOffer
 
 
 class CategoryList(generics.ListCreateAPIView):
@@ -14,4 +14,9 @@ class CategoryList(generics.ListCreateAPIView):
 class CountryList(generics.ListCreateAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+    # permission_classes = [IsAuthenticated]
+
+class JobOfferList(generics.ListCreateAPIView):
+    queryset = JobOffer.objects.all()
+    serializer_class = JobOfferSerializer
     # permission_classes = [IsAuthenticated]
